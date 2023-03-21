3a65362de2158aff63418c36371c6df302c7384d
Mateus-Gabardo <gabardomateus@gmail.com>
2023-03-20 23:00:23 -0300

    Alterado classe abstrata

--- src/main/java/Controller/TecnicoController.java
+++ src/main/java/Controller/TecnicoController.java
@@ -5,21 +5,44 @@ import java.net.Socket;
 import Model.Message;
 import Model.Tecnico;
 
-public class TecnicoController implements MessageHandler {
+public class TecnicoController extends MessageHandler {
 	
-	private Tecnico tecnico;	
+	private Tecnico tecnico;
 
-	public TecnicoController(Tecnico tecnico) {
-		super();
+	public TecnicoController(Message mensagem, Socket connexao, Tecnico tecnico) {
+		super(mensagem, connexao);
 		this.tecnico = tecnico;
 	}
 
-
-
 	@Override
-	public void processar(Message mensagem, Socket connexao) {
+	public void insert() {
 		// TODO Auto-generated method stub
-
+		
 	}
 
+	@Override
+	public void list() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void get() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
