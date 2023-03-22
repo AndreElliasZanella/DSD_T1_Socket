03780cd3343a0229adf277c77bac329a31213e7d
André E. Zanella <70669215+AndreElliasZanella@users.noreply.github.com>
2023-03-21 23:58:42 -0300

    Message

--- src/main/java/Model/Message.java
+++ src/main/java/Model/Message.java
@@ -1,71 +1,68 @@
 package Model;
 
 public class Message {
-	private Pessoa pessoa;
-	private TimeFutebol time;
-	private String metodo;
-	private String prop;
-	
-        public Message() {
-	}
-        
-	public Message(Pessoa pessoa, TimeFutebol time, String metodo) {
-		super();
-		this.pessoa = pessoa;
-		this.time = time;
-		this.metodo = metodo;
-	}
-	
-	public boolean isExistsTime() {
-		if(time != null) {
-			return true;
-		}
-		return false;
-	}
-	
-	public boolean isExistsJogador() {
-		if(pessoa != null) {
-			Class<?> obj = pessoa.getClass();
-			if(obj.equals(Jogador.class)) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public boolean isExistsTecnico() {
-		if(pessoa != null) {
-			Class<?> obj = pessoa.getClass();
-			if(obj.equals(Tecnico.class)) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public TipoMetodo getTipoMetodo(){
-		switch (metodo) {
-		case "GET": {
-			return TipoMetodo.GET;
-		}
-		case "INSERT": {
-			return TipoMetodo.INSERT;
-		}
-		case "UPDATE": {
-			return TipoMetodo.UPDATE;
-		}
-		case "DELETE": {
-			return TipoMetodo.DELETE;
-		}
-		case "LIST": {
-			return TipoMetodo.LIST;
-		}
-		default:
-			return null;
-		}
-	}
-	
-	
+    private Pessoa pessoa;
+    private Jogador jogador;
+    private TimeFutebol time;
+    private String metodo;
+    private String tipo;
+
+    public Message() {
+    }
+
+    public Message(Pessoa pessoa, TimeFutebol time, String metodo) {
+            super();
+            this.pessoa = pessoa;
+            this.time = time;
+            this.metodo = metodo;
+    }
+
+    public boolean isExistsTime() {
+            if(time != null) {
+                    return true;
+            }
+            return false;
+    }
+
+    public boolean isExistsJogador() {
+//		if(pessoa != null) {
+//			Class<?> obj = pessoa.getClass();
+//			if(obj.equals(Jogador.class)) {
+//				return true;
+//			}
+//		}
+            if(jogador != null) {
+                    return true;
+            }
+            return false;
+    }
+
+    public boolean isExistsTecnico() {
+            if(pessoa != null) {
+                    Class<?> obj = pessoa.getClass();
+                    if(obj.equals(Tecnico.class)) {
+                            return true;
+                    }
+            }
+            return false;
+    }
+
+    public Jogador getJogador() {
+        return jogador;
+    }
+
+    public void setJogador(Jogador jogador) {
+        this.jogador = jogador;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
     public Pessoa getPessoa() {
             return pessoa;
     }
